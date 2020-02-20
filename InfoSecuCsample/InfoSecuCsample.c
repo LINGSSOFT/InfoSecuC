dff53384c969a3c3eeec2301150c4d95d738ead7
nenji <jjang010@hotmail.com>
2020-02-20 11:48:37 +0900

    Add to Python Project

--- InfoSecuCsample/InfoSecuCsample.c
+++ InfoSecuCsample/InfoSecuCsample.c
@@ -1,8 +1,39 @@
-#include <stdio.h>
+#include "InfoSecuCsample.h"
 
 int main()
 {
-	// 샘플코드 연습장
+    FILE* fptr;
+    int ctr;
+    struct bookInfo books[3];
+    for (ctr = 0; ctr < 3; ctr++)
+    {
+        printf("책 #%d의 이름은?\n", (ctr + 1));
+        gets(books[ctr].title);
+        puts("책의 저자는?");
+        gets(books[ctr].author);
+        puts("책의 가격은?");
+        scanf_s("%f", &books[ctr].price);
+        puts("책의 페이지수는?");
+        scanf_s("%d", &books[ctr].pages);
+        getchar();
+    }
 
-	return 0;
+    fptr = fopen("c:\\aa\\cdata.txt", "w");
+    if (fptr == 0)
+    {
+        printf("오류-파일이 열리지 않았음.\n");
+        exit(1);
+    }
+    fprintf(fptr, "\n\n책의 컬렉션 정보: \n");
+    for (ctr = 0; ctr < 3; ctr++)
+    {
+        fprintf(fptr, "#%d: %s 저자는 %s", (ctr + 1), books[ctr].title, books[ctr].author);
+        fprintf(fptr, "\n%d 페이지와 가격은 $%.2f", books[ctr].pages, books[ctr].price);
+
+        fprintf(fptr, "\n\n");
+    }
+
+    fclose(fptr);
+    return 0;
+
 }
--- InfoSecuCsample/InfoSecuCsample.h
+++ InfoSecuCsample/InfoSecuCsample.h
@@ -1,1 +1,12 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+struct bookInfo
+{
+	char title[40];
+	char author[25];
+	float price;
+	int pages;
+};
