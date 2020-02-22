a088e1fe81cf28d8fd2c4bd834ca900f536aa08d
Nenji Jay <jjang010@hotmail.com>
2020-02-22 16:37:58 +0900

    Fix to C Algorithm Src

--- InfoSecuAlgorithm/InfoSecuAlgorithm.c
+++ InfoSecuAlgorithm/InfoSecuAlgorithm.c
@@ -3,61 +3,23 @@
 
 int main()
 {
-	// Plan
 	int nSelect = 0;
-	while (1)
-	{
-		printf("실행할 알고리즘을 선택하여주세요\n");
-		scanf_s("%d", &nSelect);
 
-		if (nSelect == 9999) break;
+	while(TRUE)
+	{
+		printf("---------------------------------\n");
+		printf("실행할 알고리즘을 선택하여주세요\n");
+		printf("1. 알고리즘 13종 \n");
+		printf("2. 종료\n");
+		printf("---------------------------------\n");
+		scanf_s("%d", &nSelect);
 
 		switch (nSelect)
 		{
 		case 1:
-			printf("GCD알고리즘\n");
-			int a = 0, b = 0;
-			int gcd = 0;
-			printf("input a : ");
-			scanf_s("%d", &a);
-			printf("input b : ");
-			scanf_s("%d", &b);
-			printf("---------------------------------\n");
-			gcd = getGCD(a, b);
-			printf("---------------------------------\n");
-			printf("result GCD : %d\n", gcd);
+			ShowAlgorithm();
 			break;
 		case 2:
-			printf("하노이알고리즘\n");
-			int n = 0;
-			printf("Input board number : ");
-			scanf_s("%d", &n);
-			printf("---------------------------------\n");
-			Hanoi(n, 'A', 'B', 'C');
-			printf("---------------------------------\n");
-			break;
-		case 3:
-			printf("소수알고리즘\n");
-			int n = 0;
-			printf("Input number : ");
-			scanf_s("%d", &n);
-			printf("---------------------------------\n");
-			if(IsPrimeNumber(n) == true)
-				printf("%d is a prime number.\n");
-			else
-				printf("%d is not prime number.\n");
-			printf("---------------------------------\n");
-			break;
-		case 4:
-			printf("마방진알고리즘\n");
-			int n = 0;
-			printf("SELECT 3,5,7 : ");
-			scanf_s("%d", &n);
-			printf("---------------------------------\n");
-			InitSquare();
-			GetOddSquare(n);
-			ShowSquare(n);
-			printf("---------------------------------\n");
 			break;
 		default:
 			printf("다시 선택해주세요!\n");
--- InfoSecuAlgorithm/InfoSecuAlgorithm.h
+++ InfoSecuAlgorithm/InfoSecuAlgorithm.h
@@ -2,3 +2,6 @@
 #define _CRT_OBSOLETE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+
+#define TRUE    1
+#define FALSE   0
